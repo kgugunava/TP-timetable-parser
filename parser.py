8b5ded121116d491550ab7a44c53c386877372cb
kgugunava <k.gugunava01@gmail.com>
2024-10-03 23:41:43 +0300

    Создал класс с методом для скачивания расписание в формате .xlsx

---
+++ parser.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlencode
+import requests
+from CONFIG import *
+
+class GetDownloadFile:
+
+    def __init__(self):
+        pass
+
+    def download(self):
+        base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+        final_url = base_url + urlencode(dict(public_key=ya_url))
+        response = requests.get(final_url)
+        download_url = response.json()['href']
+        download_response = requests.get(download_url)
+        with open('downloaded_file.xlsx', 'wb') as f:
+            f.write(download_response.content)
