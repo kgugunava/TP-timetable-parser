a5f8eed7791924dabf1f177e04a1c5ae9798d8a3
kgugunava <k.gugunava01@gmail.com>
2024-10-06 18:38:09 +0300

    Создал класс для разбиения занятий на название и тип(лекция / семинар)

---
+++ readexel.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import re
+from CONFIG import *
+
+class SplittingLessons(): # класс, для разбиения предмета на необходимые нам переменные
+
+    name_of_lesson = ''
+    type_of_lesson = '' # семинар / лекция / практическое занятие
+    teacher = ''
+
+    def __init__(self, lesson):
+        self.lesson = lesson
+
+    def get_name_of_lesson(self):
+        for cur in self.lesson: # заходим в текущий массив с массивами (lesson)
+            for cur_lesson in cur: # перебираем его элементы
+                if cur_lesson in list_of_all_lessons:
+                    self.name_of_lesson += cur_lesson + ' '
+        return self.name_of_lesson
+
+    def get_type_of_lesson(self):
+        for cur in self.lesson:
+            for cur_lesson in cur:
+                if cur_lesson in list_of_all_types_of_lessons:
+                    self.type_of_lesson = cur_lesson
+        return self.type_of_lesson
+
+    def get_teacher(self):
+        pass
+
+
+
+table = pd.read_excel('downloaded_file.xlsx', skiprows=10, header=None, keep_default_na=False)
+# чтение таблички в dataframe(объект из библиотеки pandas) (пропускаем шапку таблички, заголовков для столбцов нет, пропускаем невычисляемы/безтиповые значения)
+grouprow = [[4, 5], [7, 8], [10, 11], [16, 17], [19, 20], [22, 23], [28, 29], [31, 32], [34, 35]]
+# для каждой группы прописывам в каких столбцах лежит расписание
+for group in range(9):
+    print(group + 1)
+    x, y = grouprow[group]
+    # присваиваем значения столбцов для нужной группы
+    for index, row in table.iterrows():
+        if row[1] not in ['', 'Дни']:
+            day = row[1]
+        if row[x] != '' and row[y] != '' and row[1] != 'Дни':
+            # проверка на пустые строчки
+            time = row[2]
+            s = row[x]
+            kb = row[y].replace('\n', '').split('------')
+            s = re.sub(r'(?<=[0-9])(\.) |(?<=[0-9])(\.),', '.2024 ', s)
+            s = re.sub(r'\n(?!\n)', ' ', s)
+            s = re.sub('  ', ' ', s)
+            s = re.split("\n|---------------------", s)
+            lesson = []
+            for i in range(len(s)):
+                lesson += [re.split(' - | (?=[А-Я][^(/.)])', s[i])]
+            # с помощью регулярных выражений разбиваем строчки с расписанием на нужные категории
+            print(lesson)
+            spitted_lesson = SplittingLessons(lesson)
+            name = spitted_lesson.get_name_of_lesson()
+            type_of_lesson = spitted_lesson.get_type_of_lesson()
+            print(name, type_of_lesson)
+    print('\n\n\n')
