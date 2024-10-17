6f02a18e3e3e8478936a428ae9b57894641aca3c
kgugunava <k.gugunava01@gmail.com>
2024-10-17 16:20:10 +0300

    Update readexel.py
    Добавил разбиение по четным / нечетным неделям и реализовал добавление даты в массив

--- bd/readexel.py
+++ bd/readexel.py
@@ -1,49 +1,59 @@
 import pandas as pd
 import re
-from CONFIG import *
+from CONFIGbd import *
 
 
 class SplittingLessons():  # класс, для разбиения предмета на необходимые нам переменные
 
-    name_of_lesson = ''
-    type_of_lesson = ''  # семинар / лекция / практическое занятие
-    teacher = ''
+    name_of_lesson = []
+    type_of_lesson = []  # семинар / лекция / практическое занятие
+    teacher = []
     classroom = ''
-    campus = ''
-    day = ''
+    campus = None
+    day = None
     time = ''
+    date = []
 
 
     def __init__(self, lesson):
         self.lesson = lesson
 
     def __str__(self):
-        return f'{self.day} {self.time} {self.name_of_lesson} {self.type_of_lesson} {self.teacher} {self.campus} {self.classroom}'
+        return f'{self.date} {self.day} {self.time} {self.name_of_lesson} {self.type_of_lesson} {self.teacher} {self.campus} {self.classroom}'
 
     def get_name_of_lesson(self):
         for cur in self.lesson:  # заходим в текущий массив с массивами (lesson)
             if cur == None: # обрабатываем None, чтобы не было ошибки
+                self.name_of_lesson.append(None)
                 continue
             for cur_lesson in cur:  # перебираем его элементы
                 if cur_lesson in list_of_all_lessons:
-                    self.name_of_lesson += cur_lesson + ' '
+                    self.name_of_lesson.append(cur_lesson)
         return self.name_of_lesson
 
     def get_type_of_lesson(self):
         for cur in self.lesson:
             if cur == None: # обрабатываем None, чтобы не было ошибки
+                self.type_of_lesson.append(None)
                 continue
             for cur_lesson in cur:
                 if cur_lesson in list_of_all_types_of_lessons:
-                    self.type_of_lesson = cur_lesson
+                    self.type_of_lesson.append(cur_lesson)
         return self.type_of_lesson
 
     def get_teacher(self):
         for cur in self.lesson:
             if cur == None:
+                self.teacher.append(None)
                 continue
-            self.teacher = cur[-1]
+            self.teacher.append(cur[-1])
         return self.teacher
+
+    def refresh(self):
+        self.name_of_lesson.clear()
+        self.type_of_lesson.clear()
+        self.teacher.clear()
+        self.date.clear()
 
 
 table = pd.read_excel('timetable.xlsx', skiprows=10, header=None, keep_default_na=False)
@@ -68,7 +78,7 @@ for group in range(9):
             s = re.split("---------------------|\n", s)
             lesson = []
             for i in range(len(s)):
-                if s[i] == ' ':
+                if s[i] == ' ' or s[i] =='':
                     lesson += [None]
                 else:
                     lesson += [re.split(r' - | (?=[А-Я][^(/.)])', s[i])]
@@ -94,9 +104,14 @@ for group in range(9):
             cur_lesson.get_teacher()
             cur_lesson.day = day
             cur_lesson.time = time
+            if lesson[0] != None:
+                cur_lesson.date = lesson[0][0].split()
+            else:
+                cur_lesson.date = lesson[1][0].split()
             if len(list(filter(lambda x: x != None, kabinet))) != 0: # если массив состоит только из None, значение аудитории останется пустым
                 cur_lesson.classroom = list(filter(lambda x: x != None, kabinet))[0] # избавляюсь от элементов None и вывожу первый (и единственный) элемент массива
             if len(list(filter(lambda x: x != None, building))) != 0: # если массив состоит только из None, значение корпуса останется пустым
                  cur_lesson.campus = list(filter(lambda x: x != None, building))[0] # избавляюсь от элементов None и вывожу первый (и единственный) элемент массива
             print(cur_lesson) # вывод предмета после разбиения
+            cur_lesson.refresh()
     print('\n\n\n')
