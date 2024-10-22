6ec964c74bb8b248d1cb3b51ee66f47536e00205
kgugunava <k.gugunava01@gmail.com>
2024-10-22 21:37:45 +0300

    чет / нечет недели

--- bd/readexel.py
+++ bd/readexel.py
@@ -13,6 +13,7 @@ conn = psycopg2.connect(database="timetable",
 cur = conn.cursor()
 
 cur_number_id = 1 # для нумерования столбцов в БД
+flag = 0  # 0 - если не добавляли
 
 class SplittingLessons():  # класс, для разбиения предмета на необходимые нам переменные
 
@@ -131,14 +132,42 @@ for group in range(9):
                  cur_lesson.campus = list(filter(lambda x: x != None, building))[0] # избавляюсь от элементов None и вывожу первый (и единственный) элемент массива
             if cur_number_id == 18 or cur_number_id == 56 or cur_number_id == 73 or cur_number_id == 91 or cur_number_id == 111 or cur_number_id == 129 or cur_number_id == 147:
                 cur_lesson.name_of_lesson = ['Основы российской государственности']
-            # print(cur_lesson, cur_number_id) # вывод предмета после разбиения
+            print(cur_lesson, cur_number_id) # вывод предмета после разбиения
 
-            cur.execute(
-                '''
-                    INSERT INTO timetable VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                ''',
-                (cur_number_id, group + 1, cur_lesson.time, cur_lesson.date, cur_lesson.day, cur_lesson.name_of_lesson, cur_lesson.teacher, cur_lesson.classroom, cur_lesson.campus, 0, cur_lesson.type_of_lesson)
-            )
+            if len(cur_lesson.name_of_lesson) > 1 and cur_lesson.name_of_lesson != ['История', 'России']:
+                if cur_lesson.type_of_lesson == []:
+                    cur_lesson.type_of_lesson = ['', '']
+
+                if flag == 1:
+                    flag = 0
+                    cur_lesson.refresh()
+                    continue
+                flag = 1 # эти уроки добавляли
+                cur.execute(
+                    '''
+                        INSERT INTO timetable VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ''',
+                    (cur_number_id, group + 1, cur_lesson.time, cur_lesson.date, cur_lesson.day, cur_lesson.name_of_lesson[0], cur_lesson.teacher[0], cur_lesson.classroom, cur_lesson.campus, 0, cur_lesson.type_of_lesson[0])
+                )
+                if len(cur_lesson.teacher) == 1:
+                    cur_lesson.teacher.append('')
+                cur.execute(
+                    '''
+                        INSERT INTO timetable VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ''',
+                    (cur_number_id + 1, group + 1, cur_lesson.time, cur_lesson.date, cur_lesson.day,
+                     cur_lesson.name_of_lesson[1], cur_lesson.teacher[1], cur_lesson.classroom, cur_lesson.campus, 1,
+                     cur_lesson.type_of_lesson[1])
+                )
+                cur_number_id += 1
+                conn.commit()
+            else:
+                cur.execute(
+                    '''
+                        INSERT INTO timetable VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ''',
+                    (cur_number_id, group + 1, cur_lesson.time, cur_lesson.date, cur_lesson.day, cur_lesson.name_of_lesson, cur_lesson.teacher, cur_lesson.classroom, cur_lesson.campus, 0, cur_lesson.type_of_lesson)
+                )
             conn.commit()
             cur_number_id += 1
             cur_lesson.refresh()
