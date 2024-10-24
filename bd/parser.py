e84e3be2a5f064340c8b8001eb0db74d2e4bcfe7
deccer <burovin123@yandex.ru>
2024-10-24 12:46:10 +0300

    работа с ботом

---
+++ bd/parser.py
@@ -0,0 +1,20 @@
+from urllib.parse import urlencode
+import requests
+from CONFIG import *
+
+class GetDownloadFile:
+
+    def __init__(self):
+        pass
+
+    def download(self):
+        base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+        final_url = base_url + urlencode(dict(public_key=ya_url))
+        response = requests.get(final_url)
+        download_url = response.json()['href']
+        download_response = requests.get(download_url)
+        with open('timetable.xlsx', 'wb') as f:
+            f.write(download_response.content)
+
+downloader = GetDownloadFile()
+downloader.download()
